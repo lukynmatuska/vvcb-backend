340d52622a833773ad84becdd85f4fd23a7b50a2
Lukáš Matuška <lukynmatuska@gmail.com>
2021-08-05 00:39:29 +0200

    Add emitting on changes in ResultTemplate service

--- src/services/result-template.service.ts
+++ src/services/result-template.service.ts
@@ -1,16 +1,21 @@
 import { Inject, Injectable } from "@tsed/di";
 import { MongooseModel } from "@tsed/mongoose";
 import { ResultTemplateModel } from "src/models/result-template.model";
+import { WebSocketService } from "./web-socket.service";
 
 @Injectable()
 export class ResultTemplateService {
-    constructor(@Inject(ResultTemplateModel) private model: MongooseModel<ResultTemplateModel>) {
+    constructor(
+        @Inject(ResultTemplateModel) private model: MongooseModel<ResultTemplateModel>,
+        @Inject(WebSocketService) private wss: WebSocketService
+    ) {
 
     }
 
     async save(obj: ResultTemplateModel) {
         const doc = new this.model(obj);
         await doc.save()
+        this.wss.broadcast("new-result-template", doc);
         return doc;
     }
 
@@ -23,6 +28,8 @@ export class ResultTemplateService {
     }
 
     async deleteById(id: string) {
-        return await this.model.deleteOne({ _id: id }).exec();
+        const doc = await this.model.deleteOne({ _id: id }).exec();
+        this.wss.broadcast("delete-result-template", doc);
+        return doc;
     }
 }
