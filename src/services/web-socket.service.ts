8357876153ae49a03e689649bf95905b4598df68
Lukáš Matuška <lukynmatuska@gmail.com>
2021-08-05 00:39:09 +0200

    Add broadcast method to WS service

--- src/services/web-socket.service.ts
+++ src/services/web-socket.service.ts
@@ -1,5 +1,5 @@
-import {Injectable} from "@tsed/di";
-import { Args, Emit, Input, IO, Nsp, Socket, SocketService, SocketSession,  } from "@tsed/socketio";
+import { Injectable } from "@tsed/di";
+import { Args, Emit, Input, IO, Nsp, Socket, SocketService, SocketSession, } from "@tsed/socketio";
 import * as SocketIO from "socket.io";
 
 @Injectable()
@@ -8,18 +8,22 @@ export class WebSocketService {
 
     @Nsp nsp: SocketIO.Namespace;
 
-    constructor(@IO private io: SocketIO.Server){
+    constructor(@IO private io: SocketIO.Server) {
 
+    }
+
+    public broadcast(eventName: string, data: any) {
+        return this.nsp.emit(eventName, data);
     }
 
     @Input("message")
     @Emit("messageResponse")
-    public handleMessage(@Args(0) data: any, @Nsp socket: SocketIO.Socket){
+    public handleMessage(@Args(0) data: any, @Nsp socket: SocketIO.Socket) {
         return data;
     }
 
 
-    $onConnection(@Socket socket: SocketIO.Socket, @SocketSession session: SocketSession){
+    $onConnection(@Socket socket: SocketIO.Socket, @SocketSession session: SocketSession) {
         socket.emit("message", "Hello, World!");
         console.log("asdfasdf");
     }
